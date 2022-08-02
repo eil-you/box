66a9c9ef0406ddc4f14890e1aa0d6fd2384d9dec
SMHRD <SMHRD@DESKTOP-I5MTU0F>
2022-08-02 09:45:50 +0900

    restcontroller

---
+++ src/main/java/com/forus/controller/restController.java
@@ -0,0 +1,25 @@
+package com.forus.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.forus.mapper.UserMapper;
+
+@RestController
+public class restController {
+
+	@Autowired // self service
+	UserMapper mapper;
+
+	@RequestMapping("/CheckId.do")
+	public @ResponseBody String checkId(String user_id) {
+		String result = mapper.checkId(user_id);
+		
+		System.out.println(result);
+
+		return result;
+	}
+
+}
