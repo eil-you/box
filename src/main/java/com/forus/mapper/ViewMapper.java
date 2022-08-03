8d693b4681848e558d3a285d9f9c3c5ac1e7df98
SMHRD <SMHRD@DESKTOP-I5MTU0F>
2022-08-03 14:22:26 +0900

    dd

--- src/main/java/com/forus/mapper/ViewMapper.java
+++ src/main/java/com/forus/mapper/ViewMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.forus.domain.gCategoryVO;
-import com.forus.domain.userInfoVO;
 
 @Mapper
 public interface ViewMapper {
