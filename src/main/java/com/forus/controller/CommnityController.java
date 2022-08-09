1cda49a8d9887277bb556d1aeda291bd128b05a3
eil-you <qldirl123@gmail.com>
2022-08-09 10:47:47 +0900

    ../

--- src/main/java/com/forus/controller/CommnityController.java
+++ src/main/java/com/forus/controller/CommnityController.java
@@ -1,13 +1,18 @@
 package com.forus.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.forus.domain.CommunityVO;
 import com.forus.domain.gCategoryVO;
@@ -20,7 +25,7 @@ public class CommnityController {
 	@Autowired
 	CommunityMapper mapper;
 
-	@RequestMapping("postList,do")
+	@RequestMapping("postList.do")
 	public List<CommunityVO> postList(HttpServletRequest request, Model model) {
 		
 		
@@ -37,6 +42,30 @@ public class CommnityController {
 	}
 	
 	
+	// 게시글 등록
+	@RequestMapping("postInsert.do")
+	private void postInsert(MultipartFile file, HttpSession session, CommunityVO vo) {
+		// 주소 알기
+		String path = session.getServletContext().getRealPath("/file");
+		
+		// 이미지 저장하기
+		String uuid = UUID.randomUUID().toString();
+		// file upload to system
+		File converFile = new File(path, uuid + file.getOriginalFilename());
+		try {
+			file.transferTo(converFile);
+		} catch (IllegalStateException |IOException e) {
+			e.printStackTrace();
+		} 
+		
+		String cFile = uuid + file.getOriginalFilename();
+		vo.setArticle_file(cFile);
+		
+		// 게시글 등록 mapper
+		//mapper.articleInsert(vo);
+		
+		
+	}
 	
 	
 }
