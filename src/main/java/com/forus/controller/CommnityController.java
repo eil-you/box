3dbc2ed1c23d12f7e0c7573f8a1a801aae691698
eil-you <qldirl123@gmail.com>
2022-08-10 20:13:48 +0900

    게시글 리스트 뿌려주기

--- src/main/java/com/forus/controller/CommnityController.java
+++ src/main/java/com/forus/controller/CommnityController.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.forus.domain.CommunityVO;
+import com.forus.domain.cCategoryVO;
+import com.forus.domain.commentVO;
 import com.forus.domain.gCategoryVO;
 import com.forus.domain.goodsListVO;
 import com.forus.mapper.CommunityMapper;
+import com.forus.mapper.ViewMapper;
 
 @Controller
 public class CommnityController {
@@ -26,9 +29,13 @@ public class CommnityController {
 	@Autowired
 	CommunityMapper mapper;
 
+	
+	@Autowired
+	ViewMapper vMapper;
+	
 	// 게시글 list 
 	@RequestMapping("/postList.do")
-	public List<CommunityVO> postList(HttpSession session, Model model) {
+	public String postList(HttpSession session, Model model) {
 		
 		
 		System.out.println("호롤롤로");
@@ -40,7 +47,16 @@ public class CommnityController {
 		List<CommunityVO> result = mapper.postList(user_addr);
 		System.out.println("postList"+result);
 		
-		return result;
+		// 게시글 카테고리 뿌려주기
+		List<cCategoryVO> cVO=vMapper.communityCategory();
+		
+		
+		
+		model.addAttribute("boardList", result);
+		model.addAttribute("cCategory", cVO);
+		
+		
+		return "board";
 	}
 	
 	
@@ -75,7 +91,6 @@ public class CommnityController {
 				vo.setArticle_file(cFile);
 				}
 			
-			vo.setUser_id(user_id);
 		
 			
 			
@@ -83,15 +98,31 @@ public class CommnityController {
 			int machine_seq =mapper.machine_seqSelect(user_addr);
 			System.out.println("자판기 번호" + machine_seq);
 			vo.setV_machine_seq(machine_seq);
-			
+			vo.setUser_id(user_id);
+			System.out.println("user_id 넣은 후 vo : " +vo);
 			
 			System.out.println("vo 값 가져오기 " + vo);
 			
 			// 게시글 등록 mapper
 			mapper.articleInsert(vo);
 			
+			// 게시글 좋아요  값 insert 해주기
+			// 1) article_seq 번호 가져오기 
+			int article_seq = mapper.art_seqSelect(vo);
+			System.out.println("article_seq : " + article_seq);
+			
+			// 2) 게시글 좋아요 값 insert 해주기
+			commentVO cVO = new commentVO();
+			cVO.setArticle_seq(article_seq);
+			cVO.setUser_id(user_id);
+			cVO.setC_like(0);
+			System.out.println("게시글 좋아요 하기 위한 cVO : " +cVO);
+			int row = mapper.likeInsert(cVO);
+			System.out.println("게시글 좋아요 기본 값 결과 출력 : "  + row );
+			
+			
 		}
-		return "board";
+		return "redirect:/postList.do";
 	}
 	
 	
