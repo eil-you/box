426189cad815d7140cdee5becb4bb8c1ec76dc76
eil-you <qldirl123@gmail.com>
2022-08-11 16:33:00 +0900

    AZZ

---
+++ src/main/java/com/forus/controller/chatController.java
@@ -0,0 +1,17 @@
+//package com.forus.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class chatController {
+//	
+//	@RequestMapping("/chat")
+//	public ModelAndView chat() {
+//		ModelAndView mv = new ModelAndView();
+//		mv.setViewName("chat");
+//		return mv;
+//		
+//	}
+//}
